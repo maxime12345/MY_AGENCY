4a198a721e7ba04204410e968fa7041e1cda9199
Sebastien Balas <balas.sebastien@gmail.com>
2018-02-27 17:08:11 +0100

    routes definition

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
   resources :applications
   resources :profils, only: [:show, :edit, :update]
-
+  resources :discussions, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
