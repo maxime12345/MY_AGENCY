121cf71a998a24751fab30ba26d96d1751ee5f51
Sebastien Balas <balas.sebastien@gmail.com>
2018-02-28 18:47:02 +0100

    cloudinary, avatar on profile defined

--- app/controllers/profils_controller.rb
+++ app/controllers/profils_controller.rb
@@ -23,7 +23,7 @@ class ProfilsController < ApplicationController
   end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-  def flat_params
+  def profil_params
     params.require(:user).permit(:first_name, :last_name, :phone_number, :ID_card, :tax_notice, :payslip, :avatar, :address)
   end
 end
