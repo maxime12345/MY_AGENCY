4782c763455ce6bebaa46dff5eb294f8af6f0b31
Sebastien Balas <balas.sebastien@gmail.com>
2018-03-01 17:16:41 +0100

    candidacy index created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Candidacy.destroy_all
 Flat.destroy_all
 User.destroy_all
 #-------------------------------------------------------------------------------
