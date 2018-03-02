985308cba1082330e85264aa66dceff40aa468cd
Sebastien Balas <balas.sebastien@gmail.com>
2018-03-02 12:33:24 +0100

    Ajax messages first draft

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,42 @@
 class MessagesController < ApplicationController
-  before_action :set_candidacy, only: [:show, :edit, :update, :destroy]
-  before_action :set_flat, only: [:new, :create]
+  before_action :set_candidacy, only: [:show, :create, :edit, :update, :destroy]
 
   def index
-    @messages = Message.where()
+    if params[:flat_id].nil?
+      set_candidacy
+      @messages = policy_scope(Message).where(candidacy: @candidacy).order(created_at: :desc)
+    else
+      @candidacies = policy_scope(Candidacy).where(flat_id: params[:flat_id]).order(created_at: :desc)
+      @messages = policy_scope(Message).where(flat_id: params[:flat_id]).order(created_at: :desc)
+    end
+    @message = Message.new
   end
 
-  def new
-  end
+  # def new
+  #   @message = Message.new
+  #   authorize(@message)
+  # end
 
   def create
+    @message = Message.new(message_params)
+    @message.candidacy = @candidacy
+    @message.sender = current_user
+    if current_user == @candidacy.user
+      @message.recipient = @candidacy.flat.user
+    elsif current_user == @candidacy.flat.user
+      @message.recipient = @candidacy.user
+    end
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to candidacy_messages_path(@candidacy) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'messages/index' }
+        format.js  # <-- idem
+      end
+    end
   end
 
   def edit
@@ -23,18 +50,12 @@ class MessagesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-  def set_flat
-    @flat = Flat.find(params[:flat_id])
-    authorize(@flat)
-  end
-
   def set_candidacy
-    @candidacy = Candidacy.find(params[:id])
-    authorize(@candidacy)
+    @candidacy = Candidacy.find(params[:candidacy_id])
   end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-  def candidacy_params
-    params.permit(:flat_id, :user_id)
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
