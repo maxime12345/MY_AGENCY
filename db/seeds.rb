ac5bbf12561fd08c55ed1dc5f3789dc44090e460
Maxime Personnic <maxime.personnic@hotmail.fr>
2018-03-07 09:36:39 +0100

    update lien lbc in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ User.destroy_all
 urls = [
   "https://www.leboncoin.fr/locations/1387875755.htm?ca=12_s",
   "https://www.leboncoin.fr/locations/1385711512.htm?ca=22_s",
-  "https://www.leboncoin.fr/locations/1377652859.htm?ca=22_s",
+  "https://www.leboncoin.fr/locations/1384636419.htm?ca=5_s",
   "https://www.leboncoin.fr/locations/1395105416.htm?ca=22_s",
   "https://www.leboncoin.fr/locations/1392469156.htm?ca=22_s"
 ]
